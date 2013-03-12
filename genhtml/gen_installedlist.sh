070bb17c2f7d3dd51335fd630ddd5f7a717ffab2
Anton Bolshakov <blshkv@users.noreply.github.com>
2013-03-12 21:31:37 +0000

    genhtml: and another bit was missing

--- genhtml/gen_installedlist.sh
+++ genhtml/gen_installedlist.sh
@@ -5,13 +5,15 @@ VER="2013.0 RC1.1"
 echo "<h1>Tools List $VER </h1>"
 echo "<table class=\"desc\">"
 echo "<thead><tr><th><strong>TYPE</strong></th><th><strong>APP</strong></th><th><strong>VERSION</strong></th><th><strong>DESCRIPTION</strong></th></tr></thead>"
+echo "<tbody>"
 
 MYFR='\<tr\>
-	\<td\><category>\</td\> 
+	\<td\><category>\</td\>
 	\<td\>\<a href="<homepage>"\><name>\</a\>\</td\>
 	\<td\><installedversions:VERSION>\</td\>
 	\<td\><description>\</td\>
 \</tr\>\n'
 eix -I --pure-packages --format "$MYFR"
 
+echo "</tbody>"
 echo "</table>"
