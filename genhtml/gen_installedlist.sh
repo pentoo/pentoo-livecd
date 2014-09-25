bd8c82b0600be31d8d2b0080c4bab28ff0e7db21
Zero_Chaos <zerochaos-@users.noreply.github.com>
2014-09-25 03:43:41 +0000

    genhtml: update, make it work

--- genhtml/gen_installedlist.sh
+++ genhtml/gen_installedlist.sh
@@ -2,7 +2,7 @@
 
 cat header.inc
 
-VER="2013.0 RC1.1"
+VER="2014.0 RC3.5"
 
 echo "<h1>Tools List $VER </h1>"
 echo "<table>"
@@ -14,7 +14,7 @@ MYFR='\<tr\>
 	\<td\><installedversions:VERSION>\</td\>
 	\<td\><description>\</td\>
 \</tr\>\n'
-eix -I --pure-packages --format "$MYFR"
+EIX_LIMIT=0 HOME=/tmp eix -I --pure-packages --format "$MYFR"
 echo "</table>"
 
 cat footer.inc
