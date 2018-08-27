85e05e12cdd8fdf10005b8f14fd2f01c2c5836b8
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2018-08-27 16:22:08 -0400

    fix nvidia module creation

--- livecd/specs/make_modules.sh
+++ livecd/specs/make_modules.sh
@@ -47,7 +47,16 @@ mkdir -p /dev/shm/distfiles/rootfs/usr/portage/distfiles/
 ##if we don't clean this out it gets big.  this box doesn't reboot much
 rm -rf /dev/shm/distfiles/*
 #DISTDIR=/dev/shm/distfiles/ emerge -FO ati-drivers
-ACCEPT_LICENSE="Broadcom" DISTDIR=/dev/shm/distfiles/ emerge -FO nvidia-drivers b43-firmware b43legacy-firmware
+ACCEPT_LICENSE="Broadcom" DISTDIR="/dev/shm/distfiles/" emerge -FO b43-firmware b43legacy-firmware
+ACCEPT_LICENSE="Broadcom" DISTDIR="/dev/shm/distfiles/" ACCEPT_KEYWORDS="-* amd64" emerge -fO nvidia-drivers
+DISTDIR="/dev/shm/distfiles/" ACCEPT_KEYWORDS="-* x86" linux32 emerge -FO nvidia-drivers
+if [ -e /dev/shm/distfiles/NVIDIA-Linux-x86_64-390.67.run ]; then
+  #remove the older version of 64 bit driver
+  rm -f /dev/shm/distfiles/NVIDIA-Linux-x86_64-390.67.run
+else
+  printf "stop hacking nvidia\n"
+  exit 1
+fi
 rm -rf /dev/shm/distfiles/tmp
 mkdir -p /dev/shm/distfiles/tmp
 #cp /dev/shm/distfiles/{*[Ll]inux*,xvba*} /dev/shm/distfiles/tmp/
